0149403d7cf8866b5ca37ceff504b54fcefde4e3
IshaBansal0408 <isha.bansal1504@gmail.com>
2022-11-09 16:15:51 +0530

    Working with templates

--- First_Django_Project/HelloWorld/views.py
+++ First_Django_Project/HelloWorld/views.py
@@ -1,7 +1,23 @@
+from urllib import response
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    response = HttpResponse("Hello World")
+    # response = HttpResponse("Hello World")
+    
+    owner = "Isha Bansal"
+    allUsers = ["Kartik Gupta","Isha Bansal","Harshit Gaur","Gaurav Narula"]
+    show_Owner = True
+    context = {
+        "owner" : owner,
+        "allUsers" : allUsers,
+        "show_Owner" :show_Owner
+    }
+    
+    response = render(request,"HelloWorld/index.html",context)
+    return response
+
+def welcome(request):
+    response = render(request,'HelloWorld/welcome.html')
     return response
