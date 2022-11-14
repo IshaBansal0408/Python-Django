292da4e8067423deb539f1505da7f2aa4b072cba
IshaBansal0408 <isha.bansal1504@gmail.com>
2022-11-14 12:21:17 +0530

    added dynamic nature to project

--- 004. Portfolio Website/portfolio/settings.py
+++ 004. Portfolio Website/portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Application Specific Database
+DATA = json.load(open(os.path.join(BASE_DIR,'data.json')))
