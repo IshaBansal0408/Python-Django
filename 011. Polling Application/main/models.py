ce8883d8522ffbd2101f3fb1be186764d56900dc
IshaBansal0408 <isha.bansal1504@gmail.com>
2022-11-20 19:07:22 +0530

    poller code pushed

--- 011. Polling Application/main/models.py
+++ 011. Polling Application/main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Question(models.Model):
     content = models.CharField(max_length=256)
@@ -12,3 +12,8 @@ class Choice(models.Model):
     question = models.ForeignKey('Question',on_delete=models.CASCADE)
     def __str__(self):
         return '{}-{}'.format(self.question.content[:50],self.content)
+    
+class Answer(models.Model):
+    question = models.ForeignKey('Question',on_delete=models.CASCADE)
+    choice = models.ForeignKey('Choice',on_delete=models.CASCADE)
+    question = models.ForeignKey(User,on_delete=models.CASCADE)
--- 011. Polling Application/main/views.py
+++ 011. Polling Application/main/views.py
@@ -1,15 +1,43 @@
 from django.shortcuts import render
 from django.views.generic import(
     ListView,
-    DetailView
+    DetailView,
+    FormView,
 )
+from django.views.generic.detail import SingleObjectMixin
 from main import models
+from main import forms
+
+from django.http import HttpResponseRedirect
+
 # Create your views here.
 class Index(ListView):
     template_name = 'main/index.html'
     model = models.Question
     context_object_name = 'question_list'
     
-class Question(DetailView):
+class Question(SingleObjectMixin, FormView):
     model = models.Question
     template_name ='main/question.html'
+    form_class = forms.AnswerForm
+    
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.question = self.get_object()
+        obj.user = self.request.user
+        obj.save()
+        return HttpResponseRedirect('')
+    
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['canAnswer'] = models.Answer.objects.count(question = self.get_obect(), user= self.request.user)
+        return data
