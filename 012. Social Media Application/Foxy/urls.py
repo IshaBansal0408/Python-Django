955b896cf5f8073b848dc74b48250c118dba849a
IshaBansal0408 <isha.bansal1504@gmail.com>
2022-11-21 02:31:04 +0530

    Added social component

--- 012. Social Media Application/Foxy/urls.py
+++ 012. Social Media Application/Foxy/urls.py
@@ -2,7 +2,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from auth import urls
+from social import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/',include('auth.urls'))
+    path('auth/',include('auth.urls')),
+    path('',include('social.urls'))
 ]
--- 012. Social Media Application/social/views.py
+++ 012. Social Media Application/social/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from social import models
+
+from django.views.generic import(
+    ListView
+)
 
 # Create your views here.
+
+# will display post of all friends
+class Wall(ListView):
+    allPosts = models.Post.objects.all()
+    model = models.Post
+    context_object_name = 'allPosts'
+    template_name = 'social/wall.html'
+    
