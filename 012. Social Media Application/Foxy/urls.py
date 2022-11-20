582a9e24df018ec7791dca8b8bef84612f7365a2
IshaBansal0408 <isha.bansal1504@gmail.com>
2022-11-20 21:10:45 +0530

    Social Media App Created

---
+++ 012. Social Media Application/Foxy/urls.py
@@ -0,0 +1,8 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from auth import urls
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/',include('auth.urls'))
+]
