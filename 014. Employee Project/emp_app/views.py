0ae55cb6ce6e5bc06a38c61e70b9bb1b78b04c30
IshaBansal0408 <isha.bansal1504@gmail.com>
2022-11-30 17:59:16 +0530

    filtering data operation added

--- 014. Employee Project/emp_app/views.py
+++ 014. Employee Project/emp_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from emp_app import models
 from emp_app import forms
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import (
     CreateView,
     DetailView,
@@ -49,28 +49,27 @@ class updateEmployee(UpdateView):
 
 
 def filterEmployee(request):
-    context = {}
+    deptList = models.Department.objects.all()
+    noResult = False
+    # print(deptList)
+    if (request.method == "POST"):
+        # print("Requested Department:", request.POST.get('department'))
+        queryset = models.Employee.objects.filter(
+            department__name=request.POST.get('department'))
+        # print(queryset)
+        if (not queryset):
+            noResult = True
+        context = {
+            'department_Names': deptList,
+            'filteredEmployees': queryset,
+            'noResult': noResult
+        }
+        # return HttpResponse(queryset)
+        return render(request, 'emp_app/filterEmployee.html', context)
+    queryset = models.Employee.objects.none()
+    # print(queryset)
+    context = {
+        'department_Names': deptList,
+        'filteredEmployees': queryset
+    }
     return render(request, 'emp_app/filterEmployee.html', context)
-
-# def employees(request):
-#     allEmployess = models.Employee.objects.all()
-#     # print(allEmployess)
-#     context = {
-#         'allEmployees': allEmployess
-#     }
-#     print(context)
-#     return render(request, 'emp_app/employees.html', context)
-
-# def addEmployee(request):
-#     addForm = forms.AddEmployee()
-#     if (request.method == "POST"):
-#         addForm = forms.AddEmployee(request.POST)
-#         # print(request.POST)
-#         # print("post")
-#         if (addForm.is_valid()):
-#             addForm.save()
-#             return HttpResponseRedirect('/employees')
-#     context = {
-#         'addForm': addForm
-#     }
-#     return render(request, 'emp_app/addEmployee.html', context)
